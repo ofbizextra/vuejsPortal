b94287527524edeac207d773f733436d9528545e
bashark <j-bash@protonmail.com>
2018-07-04 21:42:50 +0200

    Ajout de VueField

--- webapp/vuejsPortal/src/components/VueField.vue
+++ webapp/vuejsPortal/src/components/VueField.vue
@@ -1,18 +1,22 @@
 <template>
-  <div id="vue-form">
-    <h2>vue-form</h2>
+  <div id="vue-field">
+    <h2>vue-field</h2>
     <h3>attributes :</h3>
     <ul>
       <li v-for="attribute in Object.keys(props.$)" :key="attribute">{{attribute + ' : ' + props.$[attribute]}}</li>
     </ul>
-    <h3>Fields :</h3>
-    <vue-field v-for="field in props.field" v-bind:props="field"/>
+    <h3>content</h3>
+    <ul>
+      <li v-for="module in Object.keys(props)" :key="module" v-if="module !== '$'">
+        {{module}}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
   export default {
-    name: "VueForm",
+    name: "VueField",
     props: ['props']
   }
 </script>
--- webapp/vuejsPortal/src/main.js
+++ webapp/vuejsPortal/src/main.js
@@ -5,6 +5,7 @@ import App from './components/App.vue'
 import Search from './components/Search.vue'
 import List from './components/List.vue'
 import VueForm from './components/VueForm'
+import VueField from './components/VueField'
 
 
 Vue.use(VueResource)
@@ -12,6 +13,7 @@ Vue.use(VueResource)
 Vue.component('search', Search)
 Vue.component('list', List)
 Vue.component('vue-form', VueForm)
+Vue.component('vue-field', VueField)
 
 new Vue({
   el: '#app',
