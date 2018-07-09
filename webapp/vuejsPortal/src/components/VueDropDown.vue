6574b641af016a160b682a0b91ef8ec176a0f407
bashark <j-bash@protonmail.com>
2018-07-09 20:01:54 +0200

    Récuperation du contenu de la dropdown

--- webapp/vuejsPortal/src/components/VueDropDown.vue
+++ webapp/vuejsPortal/src/components/VueDropDown.vue
@@ -6,12 +6,38 @@
 </template>
 
 <script>
+  import constantes from './../js/constantes'
+  import queryString from 'query-string'
+
   export default {
     name: "VueDropDown",
+    data () {
+      return {
+        options: []
+      }
+    },
     props: [
       'props',
       'name'
-    ]  }
+    ],
+    mounted () {
+      console.log(decodeURIComponent(document.cookie))
+      this.$http.post(
+        constantes.ApiUrl + constantes.listForDropDown.path,
+        queryString.stringify({
+          entityName: this.props['entity-options'][0].$['entity-name'],
+          keyFieldName: this.props['entity-options'][0].$['key-field-name'],
+          orderByFieldName: this.props['entity-options'][0]['entity-order-by'] ? this.props['entity-options'][0]['entity-order-by'][0].$['field-name'] : ''
+        }),
+        {headers: {'Content-Type': 'application/x-www-form-urlencoded'}}
+      ).then(response => {
+        console.log(response)
+        this.option = response.body.dropDownList
+      }, error => {
+        console.log(error)
+      })
+    }
+  }
 </script>
 
 <style scoped>
---
+++ webapp/vuejsPortal/src/js/constantes.js
@@ -0,0 +1,4 @@
+export default {
+  ApiUrl: 'https://localhost:8443/example/control',
+  listForDropDown: {path: '/listForDropDown', tokenRequired: false},
+}
