b6ab8fdbe3be517369e1696d37301484bced7de5
bashark <j-bash@protonmail.com>
2018-07-18 16:14:57 +0200

    modification de constante.js

--- webapp/vuejsPortal/src/js/constantes.js
+++ webapp/vuejsPortal/src/js/constantes.js
@@ -1,4 +1,6 @@
 export default {
   ApiUrl: 'https://localhost:8443/example/control',
   listForDropDown: {path: '/listForDropDown', tokenRequired: false},
+  main: {path: '/main', tokenRequired: false},
+  editExampleLayer: {path: '/EditExampleLayer', tokenRequired: false}
 }
