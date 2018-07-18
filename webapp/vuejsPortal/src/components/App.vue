5b0274ead22a5fbe22007b48ed1f1eaebf0cab10
bashark <j-bash@protonmail.com>
2018-07-18 16:07:18 +0200

    utilisation du style

--- webapp/vuejsPortal/src/components/App.vue
+++ webapp/vuejsPortal/src/components/App.vue
@@ -2,11 +2,25 @@
   <div id="app">
     <div v-for="module in Object.keys(xmlParsed)" v-bind:is="'vue-' + module" v-bind:props="xmlParsed[module]">
     </div>
+    <p>{{data}}</p>
+    <div v-for="form in parsedData" v-if="form.type === 'element' && form.tagName === 'form'" v-bind:is="form.tagName">
+      <div v-for="table in form.children" v-if="table.type === 'element' && table.tagName === 'table'" v-bind:is="table.tagName">
+        <div v-for="row in table.children" v-if="row.type === 'element' && row.tagName === 'tr'" v-bind:is="row.tagName">
+          <div v-for="cell in row.children" v-if="cell.type === 'element' && cell.tagName === 'td'" v-bind:is="cell.tagName">
+            <div v-for="dropdown in cell.children" v-if="dropdown.type === 'element' && dropdown.tagName === 'vue-drop-down'" v-bind:is="dropdown.tagName">
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
   import Xml2js from 'xml2js'
+  import constantes from './../js/constantes'
+  import {parse} from 'himalaya'
 
   export default {
   name: 'app',
@@ -41,12 +55,21 @@
       '                       target="javascript: document.FindExamples.action=\'ListExampleExport\'; document.FindExamples.submit();"/>\n' +
       '        </field>\n' +
       '    </form>',
-      xmlParsed: {}
+      xmlParsed: {},
+      data: '',
+      parsedData: {}
     }
   },
   mounted () {
     Xml2js.parseString(this.xml, (err, result) => {
       this.xmlParsed = result
+    })
+    this.$http.get(constantes.ApiUrl + constantes.editExampleLayer.path).then(response => {
+      console.log(response.body)
+      this.data = response.body
+      this.parsedData = parse(this.data)
+    }, error => {
+      console.log(error.body)
     })
   }
 }
