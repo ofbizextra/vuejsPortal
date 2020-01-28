99b2320765414c59082afdac88b5b632d3ef241d
holivier <olivier.heintz@developpement.o-toit.com>
2020-01-28 15:33:39 +0100

    for local install, create some link for new ofbiz files to be sure update is done in ofbiz and in plugin

---
+++ tools/createLinkForOfbizFiles.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+cd ../../..
+ln -s ../../plugins/vuejsPortal/ofbizFiles/docs/asciidoc/ofbiz-screenlet-reference.adoc docs/asciidoc/ofbiz-screenlet-reference.adoc
+ln -s ../../plugins/vuejsPortal/ofbizFiles/docs/asciidoc/bash.adoc docs/asciidoc/bash.adoc
+ln -s ../../plugins/vuejsPortal/ofbizFiles/docs/asciidoc/developer-manual_fr.adoc docs/asciidoc/developer-manual_fr.adoc
+ln -s ../plugins/vuejsPortal/ofbizFiles/themes/theme.adoc themes/themes.adoc
+ln -s ../../../plugins/vuejsPortal/ofbizFiles/themes/common-theme/config/iconsPurpose.properties themes/common-theme/config/iconsPurpose.properties
+ln -s ../../../plugins/vuejsPortal/ofbizFiles/framework/common/config/CommonIconsTooltips.xml framework/common/config/CommonIconsTooltips.xml
+ln -s ../../../../../../plugins/vuejsPortal/ofbizFiles/themes/common-theme/webapp/common/js/plugins/selenium.js themes/common-theme/webapp/common/js/plugins/selenium.js
