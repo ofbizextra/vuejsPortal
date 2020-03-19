1efde79b16d0764b535602c550e062b02e1a64dc
bashark <j-bash@protonmail.com>
2020-03-19 14:24:33 +0100

    Cleaning - VueTd

--- webapp/vuejs-portal/src/components/VueTd.vue
+++ webapp/vuejs-portal/src/components/VueTd.vue
@@ -1,8 +1,7 @@
-<!--suppress ALL -->
 <template>
-  <td id="vue-td" v-bind="data">
+  <td id="vue-td">
     <div
-      v-for="(component, key) in props.children"
+      v-for="(component, key) in children"
       :key="key"
       v-bind:is="constants.components[component.name]"
       :props="component"
@@ -12,24 +11,19 @@
 </template>
 
 <script>
-  import cst from '../js/constants'
+  import constants from '../js/constants'
 
   export default {
     name: "VueTd",
     props: ['props', 'updateStore'],
     data() {
       return {
-        constants: cst
+        constants: constants
       }
     },
     computed: {
-      data() {
-        let data = this.props.attributes
-        delete data['value']
-        if (data.className || (data.alert && data.alert === true)) {
-          data.class = data.className ? data.className : '' + ' ' + data.alert === true ? 'alert' : ''
-        }
-        return data
+      children() {
+        return this.props.hasOwnProperty('children') ? this.props.children : []
       }
     }
   }
