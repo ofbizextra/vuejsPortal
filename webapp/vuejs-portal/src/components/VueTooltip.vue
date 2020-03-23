a6d5124adba294d52e655415b844e7bdb5e77aef
bashark <j-bash@protonmail.com>
2020-03-23 17:36:21 +0100

    Cleaning - VueTooltip

--- webapp/vuejs-portal/src/components/VueTooltip.vue
+++ webapp/vuejs-portal/src/components/VueTooltip.vue
@@ -1,29 +1,19 @@
 <template>
-  <span id="vue-tooltip" v-if="data.tooltip" v-bind:class="tooltipStyle">
-    {{data.tooltip}}
+  <span id="vue-tooltip" v-if="tooltip" v-bind:class="tooltipStyle">
+    {{tooltip}}
   </span>
 </template>
 
 <script>
-
   export default {
     name: "VueTooltip",
     props: ['props', 'updateStore'],
-    data() {
-      return {
-      }
-    },
     computed: {
-      data() {
-        let data = this.props.attributes
-        //delete data['value']
-        if (data.className || (data.alert && data.alert === true)) {
-          data.class = data.className ? data.className : '' + ' ' + data.alert === true ? 'alert' : ''
-        }
-        return data
+      tooltip() {
+        return this.props.attributes.hasOwnProperty('tooltip') ? this.props.attributes.tooltip : ''
       },
       tooltipStyle() {
-        return this.data.hasOwnProperty('tooltipStyle') ? this.data.tooltipStyle : ''
+        return this.props.attributes.hasOwnProperty('tooltipStyle') ? this.props.attributes.tooltipStyle : ''
       }
     }
   }
