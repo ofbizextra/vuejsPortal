2a4c63e50a7ff8aaace37b9813821afc53ce11a0
bashark <j-bash@protonmail.com>
2020-03-23 17:25:11 +0100

    Cleaning - VueTextAreaField

--- webapp/vuejs-portal/src/components/VueTextAreaField.vue
+++ webapp/vuejs-portal/src/components/VueTextAreaField.vue
@@ -2,7 +2,7 @@
   <div id="vue-text-area-field">
     <v-tooltip top>
       <template v-slot:activator="{ on }">
-        <v-textarea :label="fieldTitle" v-model="value" v-bind="data" :hide-details="noRules" :rules="rules" v-on="fieldHelpText ? on : null"></v-textarea>
+        <v-textarea :label="fieldTitle" v-model="value" :hide-details="noRules" :rules="rules" v-on="fieldHelpText ? on : null"></v-textarea>
       </template>
       <span>{{fieldHelpText}}</span>
     </v-tooltip>
@@ -15,27 +15,50 @@
   export default {
     name: "VueTextAreaField",
     props: ['props', 'updateStore'],
-    data() {
-      return {}
-    },
     computed: {
       ...mapGetters({
         getForm: 'form/form',
         getDataFromForm: 'form/fieldInForm'
       }),
-      data() {
-        let data = this.props.attributes
-        //delete data['value']
-        if (data.className || (data.alert && data.alert === true)) {
-          data.class = data.className ? data.className : '' + ' ' + data.alert === true ? 'alert' : ''
+      attributesValue() {
+        return this.props.attributes.hasOwnProperty('value') ? this.props.attributes.value : ''
+      },
+      fieldHelpText() {
+        return this.props.attributes.hasOwnProperty('fieldHelpText') ? this.props.attributes.fieldHelpText : ''
+      },
+      fieldTitle() {
+        return this.props.attributes.hasOwnProperty('fieldTitle') ? this.props.attributes.fieldTitle : ''
+      },
+      formName() {
+        return this.props.attributes.hasOwnProperty('formName') ? this.props.attributes.formName : ''
+      },
+      maxLength() {
+        return this.props.attributes.hasOwnProperty('maxLength') ? this.props.attributes.maxLength : ''
+      },
+      name() {
+        return this.props.attributes.hasOwnProperty('name') ? this.props.attributes.name : ''
+      },
+      noRules() {
+        return this.required === false && this.maxLength === null
+      },
+      required() {
+        return this.props.attributes.hasOwnProperty('required') && this.props.attributes.required.hasOwnProperty('requiredField') && this.props.attributes.required.requiredField === "true"
+      },
+      rules() {
+        let rules = []
+        if (this.required) {
+          rules.push((v) => !!v || 'This field is required')
         }
-        return data
+        if (this.maxLength) {
+          rules.push((v) => v.length > this.controls.maxLength || `This field must be less than ${this.controls.maxLength} characters` )
+        }
+        return rules
       },
       storeForm() {
         return {
-          formId: this.props.attributes.formName,
-          key: this.props.attributes.name,
-          value: this.props.attributes.value ? this.props.attributes.value : ''
+          formId: this.formName,
+          key: this.name,
+          value: this.attributesValue
         }
       },
       value: {
@@ -44,45 +67,20 @@
         },
         set(value) {
           this.$store.dispatch('form/setFieldToForm', {
-            formId: this.props.attributes.formName,
-            key: this.props.attributes.name,
+            formId: this.formName,
+            key: this.name,
             value: value
           })
         }
-      },
-      controls() {
-        return {
-          required: this.data.hasOwnProperty('required') && this.data.required.hasOwnProperty('requiredField') && this.data.required.requiredField === "true",
-          maxLength: this.data.hasOwnProperty('maxLength') ? this.data.maxLength : null,
-        }
-      },
-      noRules() {
-        return this.controls.required === false && this.controls.maxLength === null && this.controls.mask === null
-      },
-      rules() {
-        let rules = []
-        if (this.controls.required) {
-          rules.push((v) => !!v || 'This field is required')
-        }
-        if (this.controls.maxLength !== null) {
-          rules.push((v) => v.length > this.controls.maxLength || `This field must be less than ${this.controls.maxLength} characters` )
-        }
-        return rules
-      },
-      fieldTitle() {
-        return this.data.hasOwnProperty('fieldTitle') ? this.data.fieldTitle : ''
-      },
-      fieldHelpText() {
-        return this.data.hasOwnProperty('fieldHelpText') ? this.data.fieldHelpText : ''
-      }
-    },
-    watch: {
-      data: function () {
-        this.$store.dispatch('form/setFieldToForm', this.storeForm)
       }
     },
     created() {
       this.$store.dispatch('form/setFieldToForm', this.storeForm)
+    },
+    watch: {
+      props: function () {
+        this.$store.dispatch('form/setFieldToForm', this.storeForm)
+      }
     }
   }
 </script>
