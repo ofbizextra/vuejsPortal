344f6347c3f899f91f2882aa44b564a455313a85
holivier <holivier@apache.org>
2020-04-15 17:58:49 +0200

    VueDateTime: remove computed about attribute not manage

--- webapp/vuejs-portal/src/components/VueDateTimeField.vue
+++ webapp/vuejs-portal/src/components/VueDateTimeField.vue
@@ -72,34 +72,10 @@
       }
     },
     computed: {
-      action() {
-        return this.props.attributes.hasOwnProperty('action') ? this.props.attributes.action : null
-      },
       ...mapGetters({
         getForm: 'form/form',
         getDataFromForm: 'form/fieldInForm'
       }),
-      alert() {
-        return this.props.attributes.hasOwnProperty('alert') ? this.props.attributes.alert : null
-      },
-      amSelected() {
-        return this.props.attributes.hasOwnProperty('amSelected') ? this.props.attributes.amSelected : ''
-      },
-      ampmName() {
-        return this.props.attributes.hasOwnProperty('ampmName') ? this.props.attributes.ampmName : ''
-      },
-      class() {
-        return this.props.attributes.hasOwnProperty('class') ? this.props.attributes.class : ''
-      },
-      className() {
-        return this.props.attributes.hasOwnProperty('className') ? this.props.attributes.className : ''
-      },
-      classString() {
-        return this.props.attributes.hasOwnProperty('classString') ? this.props.attributes.classString : ''
-      },
-      compositeType() {
-        return this.props.attributes.hasOwnProperty('compositeType') ? this.props.attributes.compositeType : ''
-      },
       config() {
         return {
           allowInput: true,
@@ -116,14 +92,8 @@
       datetime() {
         return this.date + ' ' + this.time
       },
-      dateType() {
+      dateType() {  // not yet used but will be
         return this.props.attributes.hasOwnProperty('datetype') ? this.props.attributes.datetype : 'default'
-      },
-      defaultDateTimeString() {
-        return this.props.attributes.hasOwnProperty('defaultDateTimeString') ? this.props.attributes.defaultDateTimeString : ''
-      },
-      event() {
-        return this.props.attributes.hasOwnProperty('event') ? this.props.attributes.event : ''
       },
       fieldTitle() {
         return this.props.attributes.hasOwnProperty('fieldTitle') ? this.props.attributes.fieldTitle : ''
@@ -134,38 +104,14 @@
       formName() {
         return this.props.attributes.hasOwnProperty('formName') ? this.props.attributes.formName : ''
       },
-      hour1() {
-        return this.props.attributes.hasOwnProperty('hour1') ? this.props.attributes.hour1 : ''
-      },
-      hour2() {
-        return this.props.attributes.hasOwnProperty('hour2') ? this.props.attributes.hour2 : ''
-      },
-      id() {
-        return this.props.attributes.hasOwnProperty('id') ? this.props.attributes.id : ''
-      },
       isTwelveHour() {
         return this.props.attributes.hasOwnProperty('isTwelveHour') && this.props.attributes.isTwelveHour === 'Y' ? this.props.attributes.isTwelveHour : false
-      },
-      localizedIconTitle() {
-        return this.props.attributes.hasOwnProperty('localizedIconTitle') ? this.props.attributes.localizedIconTitle : ''
-      },
-      mask() {
-        return this.props.attributes.hasOwnProperty('mask') ? this.props.attributes.mask : ''
-      },
-      maxlength() {
-        return this.props.attributes.hasOwnProperty('maxlength') ? this.props.attributes.maxlength : ''
-      },
-      minutes() {
-        return this.props.attributes.hasOwnProperty('minutes') ? this.props.attributes.minutes : ''
       },
       name() {
         return this.props.attributes.hasOwnProperty('name') ? this.props.attributes.name : ''
       },
       noRules() {
         return this.controls.required === false
-      },
-      pmSelected() {
-        return this.props.attributes.hasOwnProperty('pmSelected') ? this.props.attributes.pmSelected : ''
       },
       required() {
         return this.props.attributes.hasOwnProperty('required') ? this.props.attributes.required : {}
@@ -177,42 +123,12 @@
         }
         return rules
       },
-      shortDateInput() {
-        return this.props.attributes.hasOwnProperty('shortDateInput') ? this.props.attributes.shortDateInput : ''
-      },
-      size() {
-        return this.props.attributes.hasOwnProperty('size') ? this.props.attributes.size : ''
-      },
-      step() {
-        return this.props.attributes.hasOwnProperty('step') ? this.props.attributes.step : ''
-      },
       storeForm() {
         return {
           formId: this.formName,
           key: this.name,
           value: this.props.attributes.value ? this.props.attributes.value : ''
         }
-      },
-      tabindex() {
-        return this.props.attributes.hasOwnProperty('tabindex') ? this.props.attributes.tabindex : ''
-      },
-      timeDropdown() {
-        return this.props.attributes.hasOwnProperty('timeDropdown') ? this.props.attributes.timeDropdown : ''
-      },
-      timeDropdownParamName() {
-        return this.props.attributes.hasOwnProperty('timeDropdownParamName') ? this.props.attributes.timeDropdownParamName : ''
-      },
-      timeHourName() {
-        return this.props.attributes.hasOwnProperty('timeHourName') ? this.props.attributes.timeHourName : ''
-      },
-      timeMinutesName() {
-        return this.props.attributes.hasOwnProperty('timeMinutesName') ? this.props.attributes.timeMinutesName : ''
-      },
-      timeValues() {
-        return this.props.attributes.hasOwnProperty('timeValues') ? this.props.attributes.timeValues : ''
-      },
-      title() {
-        return this.props.attributes.hasOwnProperty('title') ? this.props.attributes.title : ''
       },
       value() {
         return this.props.attributes.hasOwnProperty('value') ? this.props.attributes.value.split('.')[0] : ''
