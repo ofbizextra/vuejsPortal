17f15e0a64b4ff96aac9078bd7ee7071791add15
holivier <holivier@apache.org>
2020-04-30 15:48:17 +0200

    VueLabel: manage the three level h1, h2, h3

--- webapp/vuejs-portal/src/components/VueLabel.vue
+++ webapp/vuejs-portal/src/components/VueLabel.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <h1 :id="id" v-if="isHeader" v-bind:is="style">{{text}}</h1>
+    <h1 :id="id" v-if="isH1" v-bind:is="style">{{text}}</h1>
+    <h2 :id="id" v-else-if="isH2" v-bind:is="style">{{text}}</h2>
+    <h3 :id="id" v-else-if="isH3" v-bind:is="style">{{text}}</h3>
     <span :id="id" v-else :class="style">{{text}}</span>
   </div>
 </template>
@@ -13,8 +15,14 @@
       id() {
         return this.props.attributes.hasOwnProperty('id') ? this.props.attributes.id : ''
       },
-      isHeader() {
-        return ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'].includes(this.style.toLocaleLowerCase())
+      isH1() {
+        return this.style.toLocaleLowerCase().split(' ').includes('h1')
+      },
+      isH2() {
+        return this.style.toLocaleLowerCase().split(' ').includes('h2')
+      },
+      isH3() {
+        return this.style.toLocaleLowerCase().split(' ').includes('h3')
       },
       style() {
         return this.props.attributes.hasOwnProperty('style') ? this.props.attributes.style : ''
